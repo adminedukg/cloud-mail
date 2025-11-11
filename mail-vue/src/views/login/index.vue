973dea70c2695c53f0d8f1fc3eed264018f6daf2
adminedukg <admin@shitou.edu.kg>
2025-11-11 22:30:58 +0800

    Update index.vue

--- mail-vue/src/views/login/index.vue
+++ mail-vue/src/views/login/index.vue
@@ -141,7 +141,7 @@
       </div>
     </el-dialog>
     <a class="github-line" href="https://mail.xxxx.us.kg/login">
-      <Icon icon="mingcute:github-line" color="#1890ff" width="20" height="20" />
+      <>
     </a>
   </div>
 </template>
